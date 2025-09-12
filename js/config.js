51e7c59b1f5dae521ea7ecbeb451f115bf6f1d85
finngid <112035527+finngid@users.noreply.github.com>
2025-09-12 13:06:20 -1000

    Update config.js
    increment by 1

--- js/config.js
+++ js/config.js
@@ -31,8 +31,8 @@ globalConfig.polyFitRejectDeviation = 3.5;
 
 // CONFIG FOR CONTINUUM SUBTRACTION
 globalConfig.polyDeg = 6;
-globalConfig.medianWidth = 102; // 51 is default 
-globalConfig.smoothWidth = 242; // 121 is default 
+globalConfig.medianWidth = 52; // 51 is default 
+globalConfig.smoothWidth = 122; // 121 is default 
 
 // QUASAR SPECIFIC CONFIG
 globalConfig.rollingPointWindow = 3;
